14b4322655743861984f2e30bd7979d2201f7be4
AceIsHuman <menoman1995@yahoo.com>
2020-09-06 19:17:18 -0500

    build display component

---
+++ src/components/Display.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const Display = (props) => {
+  return <DisplayContainer>{props.displayValue}</DisplayContainer>;
+};
+
+const DisplayContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  color: #ffffff;
+  background-color: #0f0f0f;
+  grid-row: 1;
+  grid-column: 1 / span 4;
+  font-size: 4.2rem;
+  padding: 1rem;
+  border-radius: 1rem;
+`;
+
+export default Display;
