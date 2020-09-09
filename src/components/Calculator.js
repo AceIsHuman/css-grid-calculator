7cf279211197cc5a6e9ccb8b423613e6520a83ef
AceIsHuman <menoman1995@yahoo.com>
2020-09-09 00:24:17 -0500

    add operators to calulator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,14 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-import Display from "./Display";
+import Display from './Display';
+import Operators from './Operators';
 
 const Calculator = () => {
   return (
     <CalculatorContainer>
       <Display />
+      <Operators />
     </CalculatorContainer>
   );
 };
@@ -19,7 +21,7 @@ const CalculatorContainer = styled.div`
   max-width: 30rem;
   max-height: 40rem;
   border-radius: 2rem;
-  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1.5fr 1fr 1fr 1fr 1fr 1fr;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-gap: 1rem;
   padding: 1rem;
