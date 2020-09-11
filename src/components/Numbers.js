44a306a402dfa45f4d375d334f19b9a47c6b7c8b
AceIsHuman <menoman1995@yahoo.com>
2020-09-10 21:34:58 -0500

    add handleNumber method to all buttons

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from './Button';
 
 const Numbers = props => {
+  const { handleNumber } = props;
   return (
     <>
       <Button
@@ -9,60 +10,70 @@ const Numbers = props => {
         gridRow="6 / span 1"
         gridColumn="1 / span 3"
         color="darkgray"
+        method={handleNumber}
       />
       <Button
         value="1"
         gridRow="5 / span 1"
         gridColumn="1 / span 1"
         color="darkgray"
+        method={handleNumber}
       />
       <Button
         value="2"
         gridRow="5 / span 1"
         gridColumn="2 / span 1"
         color="darkgray"
+        method={handleNumber}
       />
       <Button
         value="3"
         gridRow="5 / span 1"
         gridColumn="3 / span 1"
         color="darkgray"
+        method={handleNumber}
       />
       <Button
         value="4"
         gridRow="4 / span 1"
         gridColumn="1 / span 1"
         color="darkgray"
+        method={handleNumber}
       />
       <Button
         value="5"
         gridRow="4 / span 1"
         gridColumn="2 / span 1"
         color="darkgray"
+        method={handleNumber}
       />
       <Button
         value="6"
         gridRow="4 / span 1"
         gridColumn="3 / span 1"
         color="darkgray"
+        method={handleNumber}
       />
       <Button
         value="7"
         gridRow="3 / span 1"
         gridColumn="1 / span 1"
         color="darkgray"
+        method={handleNumber}
       />
       <Button
         value="8"
         gridRow="3 / span 1"
         gridColumn="2 / span 1"
         color="darkgray"
+        method={handleNumber}
       />
       <Button
         value="9"
         gridRow="3 / span 1"
         gridColumn="3 / span 1"
         color="darkgray"
+        method={handleNumber}
       />
     </>
   );
