f37614d9c000dcfe2a4265c4f09698af3b81563d
AceIsHuman <menoman1995@yahoo.com>
2020-09-10 21:22:40 -0500

    correct inccorect creation of styled-component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,17 +3,27 @@ import styled from 'styled-components';
 
 const Button = props => {
   const { value, gridRow, gridColumn, color, method } = props;
-  const StyledButton = styled.button`
-    grid-row: ${gridRow};
-    grid-column: ${gridColumn};
-    background-color: ${color};
-    border-radius: 1rem;
-    color: #ffffff;
-    font-size: 1.8rem;
-    font-family: 'Share Tech Mono';
-  `;
 
-  return <StyledButton onClick={method}>{value}</StyledButton>;
+  return (
+    <StyledButton
+      onClick={method}
+      gridRow={gridRow}
+      gridColumn={gridColumn}
+      color={color}
+    >
+      {value}
+    </StyledButton>
+  );
 };
+
+const StyledButton = styled.button`
+  grid-row: ${props => props.gridRow};
+  grid-column: ${props => props.gridColumn};
+  background-color: ${props => props.color};
+  border-radius: 1rem;
+  color: #ffffff;
+  font-size: 1.8rem;
+  font-family: 'Share Tech Mono';
+`;
 
 export default Button;
