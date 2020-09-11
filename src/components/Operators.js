25a0ef5042e8fc62b71cac520d6fe1c2ea4420b3
AceIsHuman <menoman1995@yahoo.com>
2020-09-10 21:54:59 -0500

    pass handleOperator into Button as method

--- src/components/Operators.js
+++ src/components/Operators.js
@@ -3,43 +3,53 @@ import React from 'react';
 import Button from './Button';
 
 const Operators = props => {
+  const { handleOperator } = props;
   return (
     <>
       <Button
         value={'C'}
+        operator="c"
         gridRow="2 / span 1"
         gridColumn="1 / span 3"
         color="red"
+        method={handleOperator}
       />
       <Button
         value={'÷'}
+        operator="/"
         gridRow="2 / span 1"
         gridColumn="4 / span 1"
         color="gray"
+        method={handleOperator}
       />
       <Button
         value={'x'}
+        operator="*"
         gridRow="3 / span 1"
         gridColumn="4 / span 1"
         color="gray"
+        method={handleOperator}
       />
       <Button
         value={'-'}
         gridRow="4 / span 1"
         gridColumn="4 / span 1"
         color="gray"
+        method={handleOperator}
       />
       <Button
         value={'+'}
         gridRow="5 / span 1"
         gridColumn="4 / span 1"
         color="gray"
+        method={handleOperator}
       />
       <Button
         value={'='}
         gridRow="6 / span 1"
         gridColumn="4 / span 1"
         color="green"
+        method={handleOperator}
       />
     </>
   );
