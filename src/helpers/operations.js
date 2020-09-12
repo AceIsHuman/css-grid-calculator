a31baf3bce1da818a1220755b48cc0207854d6ce
AceIsHuman <menoman1995@yahoo.com>
2020-09-11 23:05:32 -0500

    add operations for +,-,*,/

--- src/helpers/operations.js
+++ src/helpers/operations.js
@@ -1,6 +1,62 @@
 export default {
-  "c": props => {
+  c: props => {
     props.setTotal('0');
     props.setPreviousTotal(0);
-  }
+  },
+  '+': props => {
+    let total = props.total;
+    if (props.operation) {
+      total = evalOp(props.previousTotal, props.total, props.operation);
+      props.setTotal(`${total}`);
+    }
+    props.setPreviousTotal(total);
+    props.setOperation('+');
+  },
+  '-': props=> {
+    let total = props.total;
+    if (props.operation) {
+      total = evalOp(props.previousTotal, props.total, props.operation);
+      props.setTotal(`${total}`);
+    }
+    props.setPreviousTotal(total);
+    props.setOperation('-');
+  },
+  '*': props=> {
+    let total = props.total;
+    if (props.operation) {
+      total = evalOp(props.previousTotal, props.total, props.operation);
+      props.setTotal(`${total}`);
+    }
+    props.setPreviousTotal(total);
+    props.setOperation('*');
+  },
+  '/': props=> {
+    let total = props.total;
+    if (props.operation) {
+      total = evalOp(props.previousTotal, props.total, props.operation);
+      props.setTotal(`${total}`);
+    }
+    props.setPreviousTotal(total);
+    props.setOperation('/');
+  },
+  '=': props=> {
+    let total = props.total;
+    if (props.operation) {
+      total = evalOp(props.previousTotal, props.total, props.operation);
+      props.setTotal(`${total}`);
+    }
+    props.setPreviousTotal(total);
+    props.setOperation(null);
+  },
+};
+
+function evalOp(a, b, operation) {
+  const callOp = {
+    '+': () => parseInt(a) + parseInt(b),
+    '-': () => parseInt(a) - parseInt(b),
+    '*': () => parseInt(a) * parseInt(b),
+    '/': () => parseInt(a) / parseInt(b),
+  };
+
+  return callOp[operation]();
 }
