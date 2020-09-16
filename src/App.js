658b6480b709546a3c4f59e039a5a481c913be9c
AceIsHuman <menoman1995@yahoo.com>
2020-09-16 17:44:16 -0500

    format using prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-import Calculator from "./components/Calculator";
+import Calculator from './components/Calculator';
 
 function App() {
   return (
